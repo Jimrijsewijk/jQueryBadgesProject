4f8343e016d0568b7f71bef8e644ac7ed7d8ef2f
Jimrijsewijk <jimrijsewijk@MacBook-Air-van-Jim.local>
2016-10-15 22:56:20 +0200

    1st Try
    First upload after completing project goals

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,28 @@
 $(function() {
 
-  // your code will go here
+    // Ajax request
+    $.ajax({
+        url: 'https://www.codeschool.com/users/3000988.json',
+        dataType: 'jsonp',
+        success: function(response) {
+          console.log(response.courses.completed);
+          // handle response
+          var i;
+          var html;
+            
+          for(i =0; i < response.courses.completed.length; i++ ){
+           var course = response.courses.completed[i];   
+    
+           html = '<div class="course">';
+           html += '<h3>' + course.title + '</h3>';
+           html += '<img src="' + course.badge + '" />';
+           html += '<a href=" ' + course.url + ' " target="_blank" class="btn btn-primary" >See Course</a>';
+           html += '</div>';
+          
+           $('#badges').append(html);
+          }//for loop
+            
+        }//succes funtion
+    });
 
 });
